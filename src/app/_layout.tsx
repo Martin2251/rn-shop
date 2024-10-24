e1e66b88f634c952e4ff02d2d6f9571c450b473a
Martin Lowe <martinlowe673@gmail.com>
2024-10-24 07:28:58 +0100

    stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,9 @@
+import { Stack } from "expo-router";
+
 export default function RootLayout (){
-    
+    return(
+        <Stack>
+            <Stack.Screen name="(shop)"  options={{headerShown:true, title:"Shop"}}/>
+        </Stack>
+    )
 }
