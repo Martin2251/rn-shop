df2acce7d7a9dab61e5ed6f80dd0c178cb06a5d3
Martin Lowe <martinlowe673@gmail.com>
2024-10-26 10:10:18 +0100

    icon

--- src/app/categories/_layout.tsx
+++ src/app/categories/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack, Tabs } from "expo-router"
+import { Touchable, TouchableOpacity } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import {Ionicons} from "@expo/vector-icons"
 
 const CategoryLayout = () =>{
     return (
     <Stack>
         <Stack.Screen
         name="[slug]"
-        options={{headerShown:false}}
+        options={{headerShown:true, headerLeft:() => <TouchableOpacity>
+            <Ionicons  />
+        </TouchableOpacity>}}
         />
     </Stack>
     )
